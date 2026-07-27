9864f5bf6fc04fd1523a9e2fd8cf9e47b6e04a5e
Sarthak Gambhir <sarthak.gambhir@outlook.com>
2026-07-28 02:20:22 +0530

    Add MDX stories support to Storybook configuration

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,7 +1,7 @@
 import type { StorybookConfig } from "@storybook/react-vite";
 
 const config: StorybookConfig = {
-  stories: ["../stories/**/*.stories.@(ts|tsx)"],
+  stories: ["../stories/**/*.mdx", "../stories/**/*.stories.@(ts|tsx)"],
   addons: ["@storybook/addon-docs"],
   framework: {
     name: "@storybook/react-vite",
